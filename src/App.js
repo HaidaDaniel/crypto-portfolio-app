701bd51fd33b973c6993536ec894a62147dd051a
Daniel Haida <danilgajda22@gmail.com>
2023-09-28 16:49:10 +0300

    1st version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import Paper from '@mui/material/Paper'
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+} from '@devexpress/dx-react-grid-material-ui'
+import { useEffect, useState } from 'react'
+
+import { fetchCryptoData } from './api'
+import './App.css'
+
+const columns = [
+  {
+    name: 'rank',
+    title: 'rank',
+    width: '50px',
+  },
+  {
+    name: 'id',
+    title: 'ID',
+    width: '100px',
+  },
+  {
+    name: 'name',
+    title: 'Name',
+    width: '100px',
+  },
+  {
+    name: 'marketCapUsd',
+    title: 'marketCapUsd',
+  },
+  {
+    name: 'priceUsd',
+    title: 'priceUsd',
+    width: '200px',
+  },
+  {
+    name: 'volumeUsd24Hr',
+    title: 'volumeUsd/24Hr',
+  },
+]
 
 function App() {
+  const [rows, setRows] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetchCryptoData().then((data) => {
+      setRows(data)
+      setIsLoading(false)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Paper>
+      {isLoading && <>Loading...</>}
+      {!isLoading && (
+        <Grid rows={rows} columns={columns}>
+          <Table />
+          <TableHeaderRow />
+        </Grid>
+      )}
+    </Paper>
+  )
 }
 
-export default App;
+export default App
